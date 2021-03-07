d08c566a52fed3727b23ffa9f38217d31e32ece1
Md. Rakibul Islam <rakib1708@gmail.com>
2021-03-07 12:34:28 +0600

    Add Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('products')->delete();
+        $data = [
+            [
+                'title' => 'Test Product 1',
+                'price' => 100,
+                'description' => 'Test Product Description 1',
+                'image' => null
+            ],
+            [
+                'title' => 'Test Product 2',
+                'price' => 200,
+                'description' => 'Test Product Description 2',
+                'image' => null
+            ]
+        ];
+        Product::insert($data);
+
+        // crete dummpy products
+        Product::factory(30)->create();
+
+
+        # create user
+        DB::table('users')->delete();
+        $data = [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('123456')
+        ];
+        User::create($data);
+
     }
 }
